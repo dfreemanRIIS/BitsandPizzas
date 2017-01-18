1ba0cbf5625f56606b62a67a40c8bda4f4a4856d
dfreemanRIIS <dfreeman@riis.com>
2017-01-18 12:42:22 -0500

    Stuff

--- app/src/main/java/com/example/dfreeman/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/example/dfreeman/bitsandpizzas/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.dfreeman.bitsandpizzas;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                return true;
+            case R.id.action_setting:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
